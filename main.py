a0b7eb4cf0af43c5d85ef429c32235b3d00c3866
Vandriessche Guillaume <72578319+guillaumevd@users.noreply.github.com>
2023-02-28 15:44:49 +0100

    update

--- main.py
+++ main.py
@@ -31,20 +31,20 @@ customtkinter.set_default_color_theme("blue")
 
 window = customtkinter.CTk()
 window.iconbitmap('app/assets/icon.ico')
-window.title("Chat 4T")
-window.geometry("1280x720")
+window.title("Chat 4T v0.0.1")
+window.geometry('1280x720')
 
-label_username = customtkinter.CTkLabel(master=window, text="Pseudo :")
+label_username = customtkinter.CTkLabel(master=window, text="Pseudo :", text_color='#FFFFFF')
 label_username.place(relx=0.2, rely=0.1, anchor=tkinter.CENTER)
 
 entry_username = customtkinter.CTkEntry(master=window)
 entry_username.place(relx=0.2, rely=0.1, anchor=tkinter.CENTER)
 
 label_messages = customtkinter.CTkLabel(master=window, text="Messages :")
-label_messages.place(relx=0.5, rely=0.15, anchor=tkinter.CENTER)
+label_messages.place(relx=0.5, rely=0.1, anchor=tkinter.CENTER)
 
-text_messages = customtkinter.CTkTextbox(master=window)
-text_messages.place(relx=0.5, rely=0.2, anchor=tkinter.CENTER)
+text_messages = customtkinter.CTkTextbox(master=window, width=500, height=400)
+text_messages.place(relx=0.5, rely=0.4, anchor=tkinter.CENTER)
 
 label_message = customtkinter.CTkLabel(master=window, text="Message :")
 label_message.place(relx=0.5, rely=0.8, anchor=tkinter.CENTER)
@@ -52,7 +52,7 @@ label_message.place(relx=0.5, rely=0.8, anchor=tkinter.CENTER)
 entry_message = customtkinter.CTkEntry(master=window)
 entry_message.place(relx=0.5, rely=0.8, anchor=tkinter.CENTER)
 
-button_send = customtkinter.CTkButton(master=window, command=send_message)
+button_send = customtkinter.CTkButton(master=window, command=send_message, text='envoyer')
 button_send.place(relx=0.5, rely=0.9, anchor=tkinter.CENTER)
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
